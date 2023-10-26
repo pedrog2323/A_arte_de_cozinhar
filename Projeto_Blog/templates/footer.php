771a565e278aad08cbf4d8499bf9bc2b2d0649a4
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-10-26 14:11:00 -0300

    zs

--- Projeto_Blog/templates/footer.php
+++ Projeto_Blog/templates/footer.php
@@ -1,5 +1,14 @@
+<html>
+<body>
 <footer>
   <p>Hora de Cozinhar &copy; 2020</p>
+  <div class="center">
+    <div class="footer-t">
+      <div class="t">Instagram: [Link do Instagram]</div>
+      <div class="t">Twitter: [Link do Twitter]</div>
+      <div class="t">Pinterest: [Link do Pinterest]</div>
+    </div>
+  </div>
 </footer>
 </body>
 </html>
