6c96004458d2a21ee49f96b58bf476b79bb679bc
ryan <ryan.nascimento257@gmail.com>
2023-10-27 10:05:00 -0300

    commit dds

---
+++ Projeto_Blog/categoria.php
@@ -0,0 +1,32 @@
+<?php
+  include_once("templates/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="cat.css">
+</head>
+<body>
+</body>
+</html>
+<div class="qu" id=bra>
+    <p><strong>Culinária Brasileira</strong></p>
+    <br>
+    <p>A culinária do Brasil é fruto de uma mistura de ingredientes europeus e de povos indígenas e africanos. Muitas das técnicas de preparo e ingredientes são de origem indígena, tendo sofrido adaptações por parte dos escravos e dos portugueses.</p>
+    <br>
+    <img src="img/brazuka.jpg" id=br height="155em" width="400em" margin-bottom="5px" top="90px">
+</div>
+<div class="qu" height="70em">
+    <p><strong>Culinária Francesa</strong></p>
+    <br>
+    <p>A culinária francesa tem diversas especialidades, incluindo receitas salgadas e doces deliciosas (a famosa pâtisserie, afinal, é francesa!). Os pratos mais conhecidos incluem carnes de pato defumadas, patês diversos, peixes, queijos, crepes, cogumelos, miúdos, legumes assados, entre outras inúmeras opções</p>
+    <br>
+    <img src="img/brazuka.jpg" id=br height="155em" width="400em" margin-bottom="5px">
+</div>
+
+<?php
+  include_once("templates/footer.php")
+?> 
