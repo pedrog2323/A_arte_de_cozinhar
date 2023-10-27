483110fedfc8c68836fe01b7de7b94181056bcf7
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-27 10:16:17 -0300

    Update sobre.php

--- Projeto_Blog/sobre.php
+++ Projeto_Blog/sobre.php
@@ -2,9 +2,6 @@
   include_once("templates/header.php");
 ?>
   <h1>Página Sobre</h1>
-  <form>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti tempora itaque, vero doloribus consectetur corporis neque. Quibusdam porro, pariatur repellat minima, doloribus nemo ipsam iusto beatae hic quos quisquam! Beatae!
-</form>
 <?php
   include_once("templates/footer.php")
 ?>
