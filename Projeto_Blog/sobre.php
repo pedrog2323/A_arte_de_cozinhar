918952bf259767b1ae61dff79eeda19c23f1a5b7
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-10-27 10:21:24 -0300

    dge

--- Projeto_Blog/sobre.php
+++ Projeto_Blog/sobre.php
@@ -1,7 +1,43 @@
 <?php
   include_once("templates/header.php");
 ?>
-  <h1>Página Sobre</h1>
+<div class="main-text">
+  <h1 align="center">Página Sobre</h1>
+  <p>A arte de cozinhar: um blog para quem ama a culinária
+A arte de cozinhar é um blog de culinária. O blog é um espaço para compartilhar receitas, dicas e curiosidades sobre o mundo da culinária.</p>
+
+<p> Fla acredita que cozinhar é uma arte, e que todos podem aprender a cozinhar bem. Por isso, suas receitas são simples e fáceis de seguir, mesmo para quem não tem muita experiência na cozinha.</p>
+
+<p>No blog, você encontra receitas para todos os gostos e ocasiões, desde pratos básicos para o dia a dia até receitas mais elaboradas para ocasiões especiais. Além de receitas, a Fla também compartilha dicas de culinária, como como escolher os ingredientes certos, como temperar os pratos e como decorar as receitas.</p>
+
+<p><strong>O QUE VOCÊ ENCONTRA NO BLOG</strong></p>
+
+<p>No blog, você encontra os seguintes tipos de conteúdo:</p>
+
+<p>Receitas: receitas de pratos de diferentes culinárias, desde pratos brasileiros até pratos internacionais.</p>
+<p>Dicas: dicas de culinária, como como escolher os ingredientes certos, como temperar os pratos e como decorar as receitas.</p>
+<p>Curiosidades: curiosidades sobre o mundo da culinária, como a origem de pratos famosos ou as histórias de ingredientes e temperos.</p>
+<p><strong>COMO ACOMPANHAR O BLOG</strong></p>
+
+<p>Você pode acompanhar o blog de duas maneiras:</p>
+
+<p>Acessando o site: o blog está disponível no site www.artenacozinha.com.br.</p>
+<p>Inscreva-se na newsletter: se você quiser receber as novidades do blog diretamente no seu e-mail, inscreva-se na newsletter.</p>
+
+<p>A arte de cozinhar é um blog para quem ama a culinária. O blog é um ótimo lugar para aprender a cozinhar bem, seja você um iniciante ou um cozinheiro experiente.</p>
+
+<p>Se você gosta de cozinhar, não deixe de visitar o blog da Fla. Você vai encontrar receitas deliciosas, dicas valiosas e curiosidades interessantes sobre o mundo da culinária.</p>
+</div>
 <?php
   include_once("templates/footer.php")
 ?>
+
+<style>
+  .main-text {
+    padding: 80px;
+  }
+  .main-text > p {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+</style>
